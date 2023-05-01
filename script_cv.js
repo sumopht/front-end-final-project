87223fe5374c9dd50d35ede1e8bd8ff45c67cb1f
sumopht <ph.pitiphat@gmail.com>
2023-05-01 21:23:48 +0700

    finish display due date on table

--- script_cv.js
+++ script_cv.js
@@ -37,40 +37,172 @@ const getUserProfile = async () => {
         .catch((error) => console.error(error));
 };
 
+// first try-------------------------------
+// const createAssignmentTable = async (cv_cid) => {
+//     const table_body = document.getElementById("main-table-body");
+//     table_body.innerHTML = "";
+//     console.log("hello", cv_cid);
+//     const options = {
+//         method: "GET",
+//         credentials: "include",
+//     };
+//     let items;
+//     await fetch(
+//         `http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`,
+//         options
+//     )
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data.data);
+//             console.log("hello");
+//             items = data.data;
+//         })
+//         .catch((error) => console.error(error));
+//     items.map((item) => {
+//         console.log("hello2");
+//         const options = {
+//             method: "GET",
+//             credentials: "include",
+//         };
 
+
+//         let assignments;
+//         fetch(
+//             `http://${backendIPAddress}/courseville//get_assignment_detail/${item.itemid}`,
+//             options
+//         )
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 console.log(data);
+//                 console.log("data");
+//                 assignments = data.data;
+//             })
+//             .catch((error) => console.error(error));
+
+
+//         table_body.innerHTML += `
+//         <tr id="${item.itemid}">
+//             <td> <img src="https://www.mycourseville.com/sites/all/modules/courseville/files/thumbs/python%20logo%20trans_1532575073.png" alt=""> Computer Engineering Essentials</td>
+//             <td>${item.title}</td>
+//             <td> 17 Dec, 2022 </td>
+//             <td>
+//                 <p class="status Submitted">Submitted</p>
+//             </td>
+//         </tr>`;
+//     });
+// };
+
+// second try----------------------------------------
 const createAssignmentTable = async (cv_cid) => {
     const table_body = document.getElementById("main-table-body");
     table_body.innerHTML = "";
-    console.log("hello", cv_cid);
+    console.log("pass cv_cid succesfully");
     const options = {
         method: "GET",
         credentials: "include",
     };
     let items;
-    await fetch(
+    fetch(
         `http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`,
         options
     )
         .then((response) => response.json())
         .then((data) => {
             console.log(data.data);
-            console.log("hello");
+            console.log("fetch all assignments");
             items = data.data;
+            items.map((item) => {
+                console.log("map succesfully");
+                const options = {
+                    method: "GET",
+                    credentials: "include",
+                };
+
+
+                let assignments;
+                fetch(
+                    `http://${backendIPAddress}/courseville/get_assignment_detail/${item.itemid}`,
+                    options
+                )
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(data.data);
+                        console.log("fetch assignment detail");
+                        assignments = data.data;
+                        table_body.innerHTML += `
+                            <tr id="${item.itemid}">
+                                <td>${item.title}</td>
+                                <td>${unixTimeToDateTime(assignments.duetime)}</td>
+                                <td>
+                                <button id="addtodo-button" onclick="logout()">Add</button>
+                                </td>
+                            </tr>`;
+                    })
+                    .catch((error) => console.error(error));
+
+
+                // table_body.innerHTML += `
+                // <tr id="${item.itemid}">
+                //     <td> <img src="https://www.mycourseville.com/sites/all/modules/courseville/files/thumbs/python%20logo%20trans_1532575073.png" alt=""> Computer Engineering Essentials</td>
+                //     <td>${item.title}</td>
+                //     <td>${unixTimeToDateTime(assignments.duetime)}</td>
+                //     <td>
+                //         <p class="status Submitted">Submitted</p>
+                //     </td>
+                // </tr>`;
+            });
         })
         .catch((error) => console.error(error));
-    items.map((item) => {
-        console.log("hello2");
-        table_body.innerHTML += `
-        <tr id="${item.itemid}">
-            <td> <img src="https://www.mycourseville.com/sites/all/modules/courseville/files/thumbs/python%20logo%20trans_1532575073.png" alt=""> Computer Engineering Essentials</td>
-            <td>${item.title}</td>
-            <td> 17 Dec, 2022 </td>
-            <td>
-                <p class="status Submitted">Submitted</p>
-            </td>       
-        </tr>`;
-    });
+
 };
+
+// third try-------------------------------
+// const createAssignmentTable = async (cv_cid) => {
+//     const table_body = document.getElementById("main-table-body");
+//     table_body.innerHTML = "";
+//     console.log("pass cv_cid succesfully");
+//     const options = {
+//         method: "GET",
+//         credentials: "include",
+//     };
+//     let items;
+//     const response = await fetch(
+//         `http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`,
+//         options
+//     );
+//     const data = await response.json();
+//     console.log(data.data);
+//     console.log("fetch all assignments");
+//     items = data.data;
+//     for (const item of items) {
+//         console.log("map succesfully");
+//         const options = {
+//             method: "GET",
+//             credentials: "include",
+//         };
+
+//         const response = await fetch(
+//             `http://${backendIPAddress}/courseville/get_assignment_detail/${item.itemid}`,
+//             options
+//         );
+//         const data = await response.json();
+//         console.log(data);
+//         console.log("fetch assignment detail");
+//         const assignments = data.data;
+
+//         table_body.innerHTML += `
+//             <tr id="${item.itemid}">
+//                 <td> <img src="https://www.mycourseville.com/sites/all/modules/courseville/files/thumbs/python%20logo%20trans_1532575073.png" alt=""> Computer Engineering Essentials</td>
+//                 <td>${item.title}</td>
+//                 <td> 17 Dec, 2022 </td>
+//                 <td>
+//                     <p class="status Submitted">Submitted</p>
+//                 </td>
+//             </tr>`;
+//     }
+// };
+
+
 function handleSelectChange() {
     const cv_cid = document.getElementById("select-course").value;
     // console.log("Selected value:", cv_cid);
@@ -108,5 +240,25 @@ function initPage() {
     getUserProfile();
     showCourses();
 }
+
+const unixTimeToDateTime = (unixTimestamp) => {
+    const dateObj = new Date(unixTimestamp * 1000);
+    const months = [
+        "Jan", "Feb", "Mar", "Apr",
+        "May", "Jun", "Jul", "Aug",
+        "Sep", "Oct", "Nov", "Dec"
+    ];
+    const year = dateObj.getFullYear();
+    const month = months[dateObj.getMonth()];
+    const date = dateObj.getDate();
+    let hours = dateObj.getHours();
+    const minutes = dateObj.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours %= 12;
+    hours = hours ? hours : 12;
+    const formattedTime = `${date} ${month} ${year}, ${hours}:${minutes} ${ampm}`;
+    return formattedTime;
+};
+
 
 authorizeApplication();
